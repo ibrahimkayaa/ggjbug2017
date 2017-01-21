b16293d817e0673a36d6bfb48938df1a41017814
Onbeygir <onbeygir@hotmail.com>
2017-01-21 18:38:03 +0300

    wave stuff done

--- Assets/Scripts/EditorGrid.cs
+++ Assets/Scripts/EditorGrid.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class EditorGrid : MonoBehaviour {
 
-    public float snapValue = 1;
-    public float depth = 0;
+    [SerializeField] private float _snapValue = 1;
+    [SerializeField] private float _depth = 0;
 
     void Update()
     {
-        float snapInverse = 1 / snapValue;
-
+        float snapInverse = 1 / _snapValue;
+        
         float x, y, z;
 
         // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
         // so 1.45 to nearest .5 is 1.5
         x = Mathf.Round(transform.position.x * snapInverse) / snapInverse;
-        y = depth;  // depth from camera
+        y = _depth;  // depth from camera
         z = Mathf.Round(transform.position.z * snapInverse) / snapInverse;
+
+
 
         transform.position = new Vector3(x, y, z);
     }
---
+++ Assets/Scripts/WaveMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _minPosZ;
+    [SerializeField]
+    private float _maxPosZ;
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _waveLength;
+
+    [SerializeField]
+    private float _delay;
+    [SerializeField]
+    private float _timeItTakesToCompleteWave;
+
+    private float _currentSpeed;
+    private float _timePassed;
+
+    private bool _running;
+
+    void Start()
+    {
+        _timePassed = _delay;
+        _running = true;
+        //StartCoroutine(WaveTick());
+    }
+    
+    //IEnumerator WaveTick()
+    //{
+    //    _timePassed = 0;
+    //    int waveDirection = 1;
+    //    while (_running)
+    //    {
+    //        _timePassed += Time.deltaTime;
+    //        Mathf.Clamp01(_timePassed / _timeItTakesToCompleteWave);
+    //        _currentSpeed = Mathf.Sin();
+    //        Debug.Log(_currentSpeed);
+    //
+    //        transform.Translate(_currentSpeed * transform.forward * _speed);
+    //
+    //        yield return new WaitForSeconds(0.1f);
+    //    }
+    //}
+
+    void Update()
+    {
+        _timePassed += Time.deltaTime;
+        
+        float sined = (1 + Mathf.Sin(_timePassed * _speed))*0.5f;
+
+        //float newZ = Mathf.Clamp(sined * _minPosZ, _minPosZ, _maxPosZ);
+        float newZ = sined * _waveLength + _minPosZ;
+
+        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, newZ);
+        transform.position = newPos;
+        
+        //float dif = _lastPosZ - _startingPosZ;
+        //
+        //float move = _currentPos * dif;
+        //
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z * _currentPos);
+
+        //transform.Translate(_currentSpeed * transform.forward * _speed);
+        
+    }
+}
+
